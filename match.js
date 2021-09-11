a52aeffb92618133998a46613a34258f3986fa4b
AbdullahArbee <79457888+AbdullahArbee@users.noreply.github.com>
2021-09-11 16:41:48 +0200

    added comparison of variables and count

--- match.js
+++ match.js
@@ -1,12 +1,33 @@
-const prompt = require('prompt-sync')();
+// Bismillah
+// We first need to get the users input first
+const prompt = require('prompt-sync')({sigint: true});
+
 const first = prompt('Please enter the first string you would like to match..');
 console.log(`your first string is ${first}`);
 
 const second = prompt('Please enter the second string you would like to match...');
 console.log(`your second string is ${second}`);
 
+// We need now to slice through each first letter of both variables and count the occurrences
+for (const each of first) {
+    console.log(each);
+    const count = (first.match(/each/g) || []).length;
+    console.log(count);
+}
 
-// Old way
+for (const next of second){
+     console.log(next);
+     const again = (second.match(/next/g) || []).length;
+     console.log(again);
+}
+
+//After counting the occurrences need to bring it to 2 digits
+
+//Then get the percentage
+
+//Output the percentage with message if below or above 60%
+
+// Old way of reading input, Not good
 // const readline = require('readline').createInterface({
 //  input: process.stdin,
 //  output: process.stdout
